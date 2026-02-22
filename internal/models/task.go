dac5ec5ebfd050ad7bcb134b3368f9c70e298c15
moomien <alexspencer980@gmail.com>
2026-02-22 22:29:33 +0300

    readme.md

--- internal/models/task.go
+++ internal/models/task.go
@@ -3,7 +3,7 @@ package models
 type Task struct {
 	ID          string `json:"id"`
 	Description string `json:"description"`
-	Status      string `json:"status"` //"todo" , "in-progress", "done"
+	Status      string `json:"status"`
 	CreatedAt   string `json:"created_at"`
 	UpdatedAt   string `json:"updated_at"`
 }
